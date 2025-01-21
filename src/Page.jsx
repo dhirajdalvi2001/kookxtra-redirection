36feccd181dcbfda8b7e8da8d496e4817756af45
dhirajdalvipixelotech <dhiraj.dalvi@pixelotech.com>
2025-01-21 19:54:18 +0530

    some changes

---
+++ src/Page.jsx
@@ -0,0 +1,44 @@
+import './App.css';
+import { useSearchParams } from 'react-router-dom';
+import { useEffect } from 'react';
+
+function Page() {
+  const [searchParams] = useSearchParams();
+  const id = searchParams.get('id');
+  const isInstagram =
+    /instagram/i.test(navigator.userAgent) ||
+    /Instagram/.test(navigator.userAgent) ||
+    /InstagramWebView/.test(navigator.userAgent);
+
+  const isAndroid = /android/i.test(navigator.userAgent);
+  const isIOS = /iphone|ipad|ipod/i.test(navigator.userAgent);
+
+  console.log('User Agent:', navigator.userAgent);
+  console.log('Is Instagram:', isInstagram);
+  console.log('Is Android:', isAndroid);
+  console.log('Is iOS:', isIOS);
+  console.log('ID:', id);
+
+  useEffect(() => {
+    if (id && isInstagram) {
+      window.location.href = `intent://www.kookxtra.com/chef/?id=${id}#Intent;scheme=https;end`;
+    } else if (isAndroid) {
+      window.location.href = `https://play.google.com/store/apps/details?id=com.kookxtra.kx&pli=1`;
+    } else if (isIOS) {
+      window.location.href = `https://apps.apple.com/tt/app/kookxtra/id1597903577`;
+    }
+  }, [id, isInstagram, isAndroid, isIOS]);
+
+  return (
+    <div>
+      <p>Redirecting...</p>
+      <button>Re-Launch</button>
+      {isInstagram && <p>Opening from Instagram...</p>}
+      {isAndroid && <p>Android device detected</p>}
+      {isIOS && <p>iOS device detected</p>}
+      
+    </div>
+  );
+}
+
+export default Page;
